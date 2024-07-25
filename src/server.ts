58e7bb521aaf447e7e71fb58f95cce98c12134df
rian <rianrgomes@gmail.com>
2024-07-24 21:00:46 -0300

    feat: first solution completed successfully!

--- src/server.ts
+++ src/server.ts
@@ -10,13 +10,32 @@ const countries: string[] = [
   'colombia', 'venezuela', 'guiana', 'suriname', 'peru', 'equador', 'chile', 'argentina', 'bolivia', 'uruguai',
   'paraguai', 'brasil',
 
+  //europe
+  'dinamarca', 'islandia','irlanda', 'reino unido', 'portugal', 'espanha', 'franca', 'italia', 'andorra', 'san marino',
+  'vaticano', 'monaco', 'malta','liechtenstein', 'suica', 'austria', 'eslovenia', 'belgica', 'holanda', 'alemanha', 'luxemburgo',
+  'albania', 'montenegro', 'kosovo', 'macedonia', 'grecia', 'servia', 'hungria', 'bosnia', 'croacia', 'bulgaria', 'romenia', 'moldavia', 'ucrania', 'bielorusia', 
+  'eslovaquia', 'checa', 'polonia', 'lituania', 'letonia', 'estonia', 'finlandia', 'suecia', 'noruega', 'russia',
+
   //africa
   'madagascar', 'seychelles', 'comores', 'mauricio', 'cabo verde', 'sao tome', 'africa do sul', 'lesoto', 'essuatini',
   'zimbabue', 'namibia', 'angola', 'botswana', 'zambia', 'malawi', 'mocambique', 'congo', 'burundi', 'ruanda', 'uganda', 'tanzania',
   'quenia', 'somalia', 'etiopia', 'eritreia', 'djibuti', 'egito', 'sudao', 'sudao do sul', 'libia', 'tunisia', 'argelia',
   'marrocos', 'mauritania', 'mali', 'burkina faso', 'guine', 'guine bissau', 'senegal', 'gambia', 'serra leoa', 'liberia',
   'costa do marfim', 'gana', 'togo', 'benim', 'niger', 'nigeria', 'chade', 'camaroes', 'gabao', 'guine equatorial',
-  'africa central'
+  'africa central',
+
+  //asia
+
+  'cazaquistao', 'mongolia', 'quirguistao', 'tajiquistao', 'uzbequistao', 'turcomenistao', 'paquistao', 'afeganistao',
+  'ira', 'iraque', 'israel', 'jordania', 'siria', 'libano', 'arabia saudita', 'kuweit', 'bahrein', 'catar', 'oma',
+  'eau', 'iemen', 'georgia', 'armenia', 'azerbaijao', 'turquia', 'chipre','india', 'nepal', 'butao', 'bangladesh', 'myanmar', 'tailandia',
+  'camboja', 'vietna', 'laos', 'indonesia', 'malasia', 'singapura', 'timor leste', 'brunei', 'japao', 'coreia do norte',
+  'coreia do sul', 'china', 'taiwan', 'sri lanka', 'filipinas', 'maldivas',
+
+  //oceania
+
+  'australia', 'nova guine', 'nova zelandia', 'palau', 'nauru', 'tuvalu', 'vanuatu', 'samoa', 'salomao', 'fiji',
+  'marshall', 'kiribati', 'micronesia', 'tonga'
 ];
 
 
